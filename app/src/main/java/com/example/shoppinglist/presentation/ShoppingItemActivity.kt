69ad24a045d78c6ed27e9dd7adfe06848363be45
TDB-star <tatiana.blache@gmail.com>
2024-07-17 11:17:21 +0200

    added transition from mainActivity to shoppingItemIctivity

--- app/src/main/java/com/example/shoppinglist/presentation/ShoppingItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShoppingItemActivity.kt
@@ -1,6 +1,9 @@
 package com.example.shoppinglist.presentation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +19,28 @@ class ShoppingItemActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        Log.d("ShoppingItemActivity", mode.toString())
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOPPING_ITEM_ID = "extra_shopping_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+
+        fun newIntentAddItem(context: Context) : Intent {
+            val intent = Intent(context, ShoppingItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, itemId: Int) : Intent {
+            val intent = Intent(context, ShoppingItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOPPING_ITEM_ID, itemId)
+            return intent
         }
     }
 }
