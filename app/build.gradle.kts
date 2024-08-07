144630d19080ce9538aaa1693e350e68e7a5e466
TDB-star <tatiana.blache@gmail.com>
2024-08-07 10:03:45 +0200

    added room and coroutines dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,10 +39,13 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.kotlinx.coroutines.android)
 }
